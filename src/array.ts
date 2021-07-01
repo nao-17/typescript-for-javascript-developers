74069bcabca1b13136fcf9b1fa61205e584f02c5
nao-17 <justdo.nao01@gmail.com>
2021-07-01 20:02:24 +0900

    create array.ts

---
+++ src/array.ts
@@ -0,0 +1,17 @@
+export {};
+
+let numbers: number[] = [1, 3, 5];
+// 非推奨
+let numbers2: Array<number> =  [1, 2, 3];
+let strings2: Array<string> = ['tokyo', 'osaka'];
+
+let strings: string[] = ['typescript', 'javascript'];
+
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [19, 30],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
+
+console.log();
